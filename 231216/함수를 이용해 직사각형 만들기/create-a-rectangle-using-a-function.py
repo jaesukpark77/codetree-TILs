4a2f813116988fd72d3e33aee413f37bbab75429
JAESEOK PARK <jaesukpark77@naver.com>
2023-12-16 21:36:50 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231216/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,9 @@
+# 변수 선언 및 입력:
+row_num, col_num = tuple(map(int, input().split()))
+
+# n * m 크기의 사각형을 출력합니다.
+def print_rect(n, m):
+    for _ in range(n):
+        print("1" * m)
+
+print_rect(row_num, col_num)
