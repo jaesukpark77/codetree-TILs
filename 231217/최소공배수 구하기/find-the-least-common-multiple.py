e4959cf3b8b52981982f848a68144621f90d2ffa
JAESEOK PARK <jaesukpark77@naver.com>
2023-12-17 00:00:35 +0900

    Title: 최소공배수 구하기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231217/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,13 @@
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+
+# n과 m의 최소공배수를 출력합니다.
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(n * m // gcd)
+   
+find_lcm(n, m)
